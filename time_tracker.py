fd104fdd3880150974dba8f7529faab95981e3e3
Ashley Turvey <at23291@my.westlake.school.nz>
2026-05-27 09:51:03 +1200

    worked on validate input and add entry page

--- time_tracker.py
+++ time_tracker.py
@@ -14,18 +14,60 @@ def save_data(data):
         json.dump(data, file, indent = 4)
 
 #Check to make sure that the input is able to be used
-def validate_input(input):
-    print("placeholder")
+def validate_input(input_):
+    while True:
+        value = easygui.enterbox(input_)
+
+        if value is None:
+            msgbox("Going home...")
+            home_page()
+        
+
+
+        try:
+            minutes = float(value)
+
+            if minutes <= 0:
+                msgbox("Cant enter negative numbers.")
+                continue
+
+            elif minutes >= 1440:
+
+
+        except ValueError:
+            counter += 1
+            if counter < 5:
+                msgbox("That's not a number. Try again.")
+
 
 
 #Make a time entry
 def add_activity_page():
+    #building a list with the ways people spend their time.
     activities = ["Study/Work", "Sport", "Music", "Screen", "Family/Friends", \
 "Other"]
-    activities_select = easygui.choicebox("Which catagory would you like\
- to make an entry for?", "activities_select", activities)
-    activities_hours = easygui.enterbox(f"How many minutes did you spend doing\
- {activities_select}?")
+    
+    #letting them pick one
+    activities_select = easygui.choicebox("Which catagory would you like \
+to make an entry for?", "activities_select", activities)
+    
+    #checking to make sure they clicked one, and making them do it again
+    #if not
+    if activities_select is None:
+        easygui.msgbox("Exiting...")
+        home_page()
+
+    else:
+        activities_hours = validate_input(f"How many minutes did you spend \
+doing {activities_select}?")
+        
+        if activities_hours is None:
+            print("none")
+        else:
+            return activities_hours, activities_select
+
+
+    return activities_select, activities_hours
 
 #Show either todays daily summary or another days summary
 def history_page():
